7c17beed892ed309aaa9cdddc61c673240dae963
raisiglPeter <p.raisigl@gmail.com>
2025-08-13 09:22:17 -0400

    add mobile menu, remove hiRes img, fixes

--- script/script.js
+++ script/script.js
@@ -1,1 +1,15 @@
 "use strict";
+
+const hamburgerButton = document.querySelector(".hamburger");
+const hamburgerMenu = document.getElementById("hamburger-menu");
+const closeButton = document.querySelector(".hamburger-menu__close");
+
+hamburgerButton.addEventListener("click", () => {
+  hamburgerMenu.classList.add("open");
+  hamburgerButton.classList.add("display-none");
+});
+
+closeButton.addEventListener("click", () => {
+  hamburgerMenu.classList.remove("open");
+  hamburgerButton.classList.remove("display-none");
+});
